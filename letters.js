1537173eaab19fde51b3f725e80205e5a9b20520
fcortesbio <fcortesbio@gmail.com>
2025-04-21 14:32:59 -0500

    docs: Included description of the challenge

--- letters.js
+++ letters.js
@@ -1,4 +1,18 @@
 /**
+🧠 Challenge: Letter Score Counter
+Write a function that calculates the total score of a word based on this rule:
+Each letter has a value equal to its position in the alphabet:
+A/a = 1, B/b = 2, ..., Z/z = 26
+
+Your function should:
+* Take a string as input.
+* Ignore non-letter characters (e.g., punctuation, numbers).
+* Be case-insensitive.
+* Return the total score of all valid letters.
+
+*/
+
+/**
  * Calculates the total score of a word based on the position of each letter in the alphabet.
  * Non-alphabetic characters are ignored. The letter 'ñ' is treated as 'n'.
  *
@@ -20,5 +34,4 @@ function scoreWord(input){
     return score
 }
 
-// testing
 console.log("Score of 'Hello, World!': ", scoreWord("Hello, World!"))
