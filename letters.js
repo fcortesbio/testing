559c390f1ca9b2ebf83cd476360bab057a3214a1
fcortesbio <fcortesbio@gmail.com>
2025-04-21 13:56:00 -0500

    docs: Add JSDoc comments for the scoreWord function

--- letters.js
+++ letters.js
@@ -1,18 +1,19 @@
-
 /**
- * Calculates the total score of a word based on the position of each letter in the alphabet
- * @param {String} input The string to be scored
- * @returns {Number} The numerical score of the evaluated word
-*/
+ * Calculates the total score of a word based on the position of each letter in the alphabet.
+ * Non-alphabetic characters are ignored. The letter 'ñ' is treated as 'n'.
+ *
+ * @param {string} input The string to be scored.
+ * @returns {number} The numerical score of the evaluated word.
+ */
 function scoreWord(input){
     let score = 0;
     const letters = [..."abcdefghijklmnopqrstuvwxyz"];
-    
+
     [...input.trim().toLowerCase()].forEach(letter => {
         if (letters.includes(letter)){
             score += letters.indexOf(letter) + 1;
         }
-        else if (letter == "ñ"){
+        else if (letter === "ñ"){
             score += letters.indexOf("n") + 1;
         }
     })
